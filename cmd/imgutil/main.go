9e95abecce71741f5c08c8863695bdb91e6bf75a
Ashwin Venkatesh <ashwin@Ashwins-MacBook-Pro.local>
2026-02-19 12:11:05 -0700

    feat: wire up main entry point and complete CLI bootstrap

--- cmd/imgutil/main.go
+++ cmd/imgutil/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/thisisnotashwin/imgutil/commands"
+	"github.com/thisisnotashwin/imgutil/internal/image"
+)
 
 func main() {
-	fmt.Println("imgutil")
+	loader := image.NewLoader()
+	if err := commands.NewRootCmd(loader).Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
